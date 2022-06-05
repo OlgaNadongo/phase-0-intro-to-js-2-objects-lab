9f968bf15e461c6aa3cf999ac4e9308b0e9d36e3
Olga Nadongo <winnienadongo@gmail.com>
2022-06-05 08:54:28 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+const employee={
+    name: "Kia",
+    streetAddress: "11 Nairobi",
+};
+function updateEmployeeWithKeyAndValue(){
+     const copyOfEmployee= {
+       
+       name: "Sam",
+       streetAddress: "11 Broadway",
+ 
+     }
+     return copyOfEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+     employee.streetAddress="12 Broadway";
+     const newEmployee= employee
+    return employee
+ }
+ 
+function deleteFromEmployeeByKey(){
+    const copyOfEmployee1={
+        streetAddress:"11 Broadway"
+    }
+    return copyOfEmployee1
+}
+  
+   
+
+function destructivelyDeleteFromEmployeeByKey(){
+     
+    delete employee['name']
+    return employee
+}
+  
